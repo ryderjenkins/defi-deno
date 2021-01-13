eb2bd435ddc52debc911a8487412facdb38519fe
ryderjenkins <ryderjenkins11@gmail.com>
2021-01-14 00:15:52 +0100

    fix

--- src/test-it.ts
+++
@@ -1,6 +0,0 @@
-// import { DeFiService } from "https://deno.land/x/defi/defi.service.ts"
-import { DeFiService } from "./defi.service.ts"
-
-const gasPriceInfo = await DeFiService.getGasPriceInfo()
-
-console.log(gasPriceInfo)
---
+++ test-it.ts
@@ -0,0 +1,6 @@
+import { DeFiService } from "https://deno.land/x/defi/defi.service.ts"
+// import { DeFiService } from "./defi.service.ts"
+
+const gasPriceInfo = await DeFiService.getGasPriceInfo()
+
+console.log(gasPriceInfo)
